9a332c67484a5e4317fa6e12544daab89a2f3dca
Maureenm41 <maureenmrchl@gmail.com>
2021-05-10 09:39:56 +0200

    this a test

--- server.js
+++ server.js
@@ -12,3 +12,28 @@ const server = http.createServer((req, res) => {
   });
 
 console.log('Hello World!')
+
+const fs = require('fs');
+
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+        if (err) throw err
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end(data);
+    }) 
+  
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+const url = require('url');
+
+const query = url.parse(req.url, true).query;
